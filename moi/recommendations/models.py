8297fd0ceb52599506c240e8d5d7081324ebbe19
Ryan Hodges <ryan.d.hodges@gmail.com>
2015-12-29 16:26:57 -0800

    updating pages with some basic fields. Can change later. Must migrate!

--- moi/recommendations/models.py
+++ moi/recommendations/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.models import Image
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 class Recommendation(Page):
+    displayTitle = RichTextField(blank=True)
+    sub_title = RichTextField(blank=True, null=True, default=None)
+    main_content = RichTextField(blank=True, null=True, default=None)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('displayTitle', classname="full"),
+        FieldPanel('sub_title', classname="subtitle or blurb"),
+        FieldPanel('main_content', classname="content"),
+        ImageChooserPanel('image')
+    ]
+
     parent_page_types = ['indicators.Indicator']
-    # subpage_types = []
-    # page_type = models.CharField(default='Recommendation', max_length=255)
 
     class Meta:
         verbose_name = 'Recommendation'
-
-    # def get_context(self, request):
-    #     context = super(Recommendation, self).get_context(request)
-    #
-    #     context['base_page_type'] = 'Recommendation'
-    #     return context
--- moi/sectors/models.py
+++ moi/sectors/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.models import Image
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 class Sector(Page):
+    displayTitle = RichTextField(blank=True)
+    sub_title = RichTextField(blank=True, null=True, default=None)
+    main_content = RichTextField(blank=True, null=True, default=None)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('displayTitle', classname="full"),
+        FieldPanel('sub_title', classname="subtitle or blurb"),
+        FieldPanel('main_content', classname="content"),
+        ImageChooserPanel('image')
+    ]
+
     parent_page_types = ['data.Data']
     subpage_types = ['outcomes.PriorityOutcome']
