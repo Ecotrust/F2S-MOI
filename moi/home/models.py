fe098fbb54d07cc5cda5431c5e744c2f2741b556
Drew <drew@pointnineseven.com>
2016-01-20 17:00:50 -0800

    setup homepage for sectors and top stories

--- moi/home/models.py
+++ moi/home/models.py
@@ -2,15 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
 from wagtail.wagtailimages.models import Image
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    displayTitle = RichTextField(blank=True)
+    display_title = RichTextField(blank=True)
     sub_title = RichTextField(blank=True, null=True, default=None)
     main_content = RichTextField(blank=True, null=True, default=None)
     image = models.ForeignKey(
@@ -23,10 +25,38 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('displayTitle', classname="full"),
+        FieldPanel('display_title', classname="full"),
         FieldPanel('sub_title', classname="subtitle or blurb"),
         FieldPanel('main_content', classname="content"),
-        ImageChooserPanel('image')
+        ImageChooserPanel('image'),
+        InlinePanel('top_stories', label="Top Stories"),
     ]
 
     subpage_types = ['about.About', 'carousel.Carousel', 'data.Data']
+
+#Top Stories
+class TopStories(models.Model):
+    sector_name = RichTextField(blank=True, help_text="Sector Name - Headline")
+    top_stories_content = RichTextField(blank=True, help_text="Main content field for top stories")
+    link_caption = models.CharField(max_length=250, help_text="Text for clickable links to specific sector")
+    url = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+',
+        help_text="Internal path to specific sector",
+    )
+
+    panels = [
+        FieldPanel('sector_name'),
+        FieldPanel('top_stories_content'),
+        FieldPanel('link_caption'),
+        PageChooserPanel('url'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class HomePageTopStories(Orderable, TopStories):
+    page = ParentalKey('HomePage', related_name='top_stories')
