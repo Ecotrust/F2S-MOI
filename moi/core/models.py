1a8ea90860ef5a3c5444faa8585a6e3432372767
Drew <drew@pointnineseven.com>
2016-01-22 15:59:47 -0800

    more streamin' - now with two columns

--- moi/core/models.py
+++ moi/core/models.py
@@ -11,9 +11,10 @@ from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
 from wagtail.wagtailsearch import index
 
 from wagtail.wagtailcore.blocks import TextBlock, StructBlock, StreamBlock, FieldBlock, CharBlock, \
-     RichTextBlock, RawHTMLBlock
+     ChoiceBlock, RichTextBlock, RawHTMLBlock
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+from wagtail.wagtailembeds.blocks import EmbedBlock
 
 from modelcluster.fields import ParentalKey
 
@@ -52,6 +53,26 @@ class AlignedHTMLBlock(StructBlock):
     class Meta:
         icon = "code"
 
+class TwoColumnBlock(StructBlock):
+    
+    left_column = StreamBlock([
+            ('heading', CharBlock(icon="title", classname="full title")),
+            ('paragraph', RichTextBlock(icon="pilcrow")),
+            ('image', ImageChooserBlock(icon="image")),
+            ('embedded_video', EmbedBlock(icon="media")),
+        ], icon='arrow-left', label='Left content')
+ 
+    right_column = StreamBlock([
+            ('heading', CharBlock(icon="title", classname="full title")),
+            ('paragraph', RichTextBlock(icon="pilcrow")),
+            ('image', ImageChooserBlock(icon="image")),
+            ('embedded_video', EmbedBlock(icon="media")),
+        ], icon='arrow-right', label='Right content')
+ 
+    class Meta:
+        template = 'blocks/two_column_block.html'
+        icon = 'placeholder'
+        label = 'Two Columns'
 
 class CoreStreamBlock(StreamBlock):
     h2 = CharBlock(icon="title", classname="title")
@@ -63,3 +84,6 @@ class CoreStreamBlock(StreamBlock):
     pullquote = PullQuoteBlock()
     aligned_html = AlignedHTMLBlock(icon="code", label='Raw HTML')
     document = DocumentChooserBlock(icon="doc-full-inverse")
+    embedded_video = EmbedBlock(icon="media")
+    two_column = TwoColumnBlock() 
+
