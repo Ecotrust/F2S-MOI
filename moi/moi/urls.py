4a6d7fb846887b192071cac35d266a3a8bf52e50
Drew Seminara <dnseminara@gmail.com>
2016-02-13 09:16:19 -0800

    create quick splash page now that url is public

--- moi/moi/urls.py
+++ moi/moi/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -8,6 +9,7 @@ from wagtail.wagtailcore import urls as wagtail_urls
 
 
 urlpatterns = [
+    url(r'^(?!(django-admin|admin))$',TemplateView.as_view(template_name="moi/splash.html")),
     url(r'^django-admin/', include(admin.site.urls)),
 
     url(r'^admin/', include(wagtailadmin_urls)),
