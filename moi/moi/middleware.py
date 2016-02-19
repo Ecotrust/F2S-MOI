cc652efabf8b0b61b52267e4ff05a858bef332de
Drew <drew@pointnineseven.com>
2016-02-19 12:39:53 -0800

    update middleware to allow static files when not logged in

--- moi/moi/middleware.py
+++ moi/moi/middleware.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 
 class AuthRequiredMiddleware(object):
     def process_request(self, request):
-        if '/admin' not in request.path and request.user.is_anonymous():
+        if '/admin' not in request.path and settings.STATIC_URL not in request.path and request.user.is_anonymous():
             return render(request, 'moi/splash.html')
         return None
