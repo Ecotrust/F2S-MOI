5c58b1726e85c4dd277364407732fe85b2712edf
Drew <drew@pointnineseven.com>
2016-04-13 15:59:25 -0700

    bind data model to core measures

--- moi/data/models.py
+++ moi/data/models.py
@@ -20,36 +20,45 @@ class Data(models.Model):
                                        max_length=1,
                                        null=True,
                                        default="",
+                                       verbose_name='Data Viz Type',
                                        help_text='Select the type of data vizulation for this Core Measure')
-    prime_label = models.CharField(blank=True, 
+    prime_label = models.TextField(blank=True, 
                                    null=True,
                                    default="", 
-                                   max_length=255, 
-                                   help_text='This will be primary label/text for the data vizulation')
+                                   verbose_name='Prime Label', 
+                                   help_text='This will be primary label/text for the data visualization')
+    source = models.TextField(blank=True, 
+                              null=True,
+                              default="", 
+                              help_text='Source text for data visualization')
     year = models.CharField(blank=True,
                             null=True,
                             default="",
                             max_length=255,
-                            help_text='Input the year/date range for the associated data vizulation')
+                            help_text='Input the year/date range for the associated data visualization')
     data_values = models.CharField(blank=True, 
                              null=True,
                              default="", 
-                             max_length=255, 
+                             max_length=255,
+                             verbose_name='Data', 
                              help_text='Enter your values here. If there is more than one value present, separate them by a semicolon')
     data_labels = models.CharField(blank=True, 
                              null=True,
                              default="", 
-                             max_length=255, 
+                             max_length=255,
+                             verbose_name="Data Labels", 
                              help_text='Enter your labels here. If there is more than one label present, separate them by a semicolon')
     x_axis_label = models.CharField(blank=True, 
                              null=True,
                              default="", 
-                             max_length=255, 
+                             max_length=255,
+                             verbose_name='X-Axis Label', 
                              help_text='Enter your x-axis label here')
     y_axis_label = models.CharField(blank=True, 
                              null=True,
                              default="", 
-                             max_length=255, 
+                             max_length=255,
+                             verbose_name='Y-Axis Label', 
                              help_text='Enter your y-axis label here')
 
 
@@ -64,3 +73,5 @@ class Data(models.Model):
         FieldPanel('y_axis_label'),
     ]
 
+    class Meta:
+        abstract = True
--- moi/measures/models.py
+++ moi/measures/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField, StreamField
-from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.wagtailsearch import index
+
+from data.models import Data as DV
 
 from core.models import CoreStreamBlock
 
-class CoreMeasure(Page):
-    DATA_CHOICES = (
-        ('pie', 'Pie'),
-        ('bar', 'Bar'),
-        ('line', 'Line'),
-        ('number', 'Number'),
-    )
+class RelatedData(DV):
+    content_panels = [ MultiFieldPanel(DV.content_panels, 'Data'), ]
 
+    class Meta:
+        abstract = True
+
+
+class CoreMeasure(Page):
     display_title = RichTextField(blank=True)
     body_content = StreamField(CoreStreamBlock(), blank=True, null=True, default=None)
-    data_viz_type = models.CharField(choices=DATA_CHOICES,
-                                    max_length=1,
-                                    null=True,
-                                    default=None,
-                                    help_text='Select the type of data vizulation for this Core Measure')
 
     search_fields = Page.search_fields + (
             index.SearchField('display_title'),
@@ -30,11 +29,15 @@ class CoreMeasure(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('display_title'),
-        FieldPanel('data_viz_type'),
         StreamFieldPanel('body_content'),
+        InlinePanel('related_data', label='Data'),
     ]
 
     parent_page_types = ['indicators.Indicator']
 
     class Meta:
         verbose_name = "Core Measure"
+
+
+class CoreMeasureRelatedData(RelatedData):
+    page = ParentalKey('CoreMeasure', related_name='related_data')
