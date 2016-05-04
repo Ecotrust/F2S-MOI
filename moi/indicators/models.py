39453dbdaa92a0713deaf53c32b5538148356aaa
Drew <drew@pointnineseven.com>
2016-05-04 14:33:52 -0700

    make priority outcomes and indicators orderable

--- moi/indicators/models.py
+++ moi/indicators/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField, StreamField
 from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel
 from wagtail.wagtailsearch import index
 
 from core.models import CoreStreamBlock
 
-class Indicator(Page):
+class Indicator(Page, Orderable):
     display_title = RichTextField(blank=True)
     body_content = StreamField(CoreStreamBlock(), blank=True, null=True, default=None)
 
