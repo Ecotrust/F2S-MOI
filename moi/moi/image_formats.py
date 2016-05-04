330e87ae8af70b0bd886061d5f4b627196b8b998
Drew <drew@pointnineseven.com>
2016-05-04 12:26:48 -0700

    modify text on extra-content and default image format

--- moi/moi/image_formats.py
+++ moi/moi/image_formats.py
@@ -1,7 +1,7 @@
 from wagtail.wagtailimages.formats import Format, register_image_format, unregister_image_format
 
 unregister_image_format('fullwidth')
-register_image_format(Format('fullwidth', 'Full width', 'richtext-image full-width img-responsive', 'width-800'))
+register_image_format(Format('fullwidth', 'Full width', 'richtext-image full-width img-responsive', 'width-850'))
 
 unregister_image_format('left')
 
@@ -11,4 +11,4 @@ register_image_format(Format('center', 'Centered', 'richtext-image img-responsiv
 
 register_image_format(Format('multiple', 'Multiple Images', 'richtext-image img-responsive multi', 'original'))
 
-register_image_format(Format('admin', 'Admin Default', 'richtext-image img-responsive', 'max-800x600'))
+register_image_format(Format('admin', 'Admin Default', 'richtext-image img-responsive center-block', 'max-600x450'))
