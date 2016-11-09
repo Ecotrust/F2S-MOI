c649395ef403362e8eba09924cb0849c1e554364
David Pollard <dpollard@ecotrust.org>
2016-11-09 10:01:07 -0800

    update env settings

--- moi/manage.py
+++ moi/manage.py
@@ -4,8 +4,6 @@ import sys
 
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moi.settings")
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moi.settings.dev")
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moi.settings.production_template")
 
     from django.core.management import execute_from_command_line
 
